0d605116d0c6f3a99fd812652fa5cd7c17c9c09a
nstoyanov <n.stoianov@alcorosoft.bg>
2020-10-16 20:22:55 +0300

    Add AppDbContext and ApplicationUser

---
+++ AutomobileProject/AutomobileProject/Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using AutomobileProject.Data.Models.User;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutomobileProject.Data.Models
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext()
+        {
+
+        }
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            :base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
