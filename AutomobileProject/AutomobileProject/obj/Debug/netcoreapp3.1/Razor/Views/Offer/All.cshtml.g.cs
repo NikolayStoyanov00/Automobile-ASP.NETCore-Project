542688360bcad885572fc22e850dce07b27f64c9
NikolayStoyanov00 <nikolai_kula1@abv.bg>
2020-10-23 10:23:01 +0300

    Add bootstrap template, minor CSS Add Offer fixes, Add new Logo

--- AutomobileProject/AutomobileProject/obj/Debug/netcoreapp3.1/Razor/Views/Offer/All.cshtml.g.cs
+++ AutomobileProject/AutomobileProject/obj/Debug/netcoreapp3.1/Razor/Views/Offer/All.cshtml.g.cs
@@ -27,7 +27,7 @@ using AutomobileProject.ViewModels;
 #line hidden
 #nullable disable
     [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"57218c316b6921e2cd61027a2387edc31a2d9471", @"/Views/Offer/All.cshtml")]
-    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"b500941621bf8397076d6131512f82e2bd771959", @"/Views/_ViewImports.cshtml")]
+    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"88d1fae58ed41a16ce176636fa13e1b39025dd44", @"/Views/_ViewImports.cshtml")]
     public class Views_Offer_All : global::Microsoft.AspNetCore.Mvc.Razor.RazorPage<dynamic>
     {
         #pragma warning disable 1998
